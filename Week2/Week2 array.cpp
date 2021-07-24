af2923d9a05b6520fbee4ae4df2eb33a86f46603
Yash Vardhan Tyagi <77291864+yashvardhan-rustedlegend@users.noreply.github.com>
2021-07-24 13:19:21 +0530

    code

---
+++ Week2/Week2 array.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int testcase;
+	cout<<"enter the no. of test cases";
+	cin>>testcase;
+	while(testcase--)
+	{
+		int size,element,count=0;
+		cout<<"Enter the size of array";
+		cin>>size;
+		int arr[size];
+		for(int i=0;i<size;i++)
+		{
+			cin>>arr[i];
+		}
+		cout<<"Search Element=";
+		cin>>element;
+		for(int i=0;i<size;i++)
+		{
+			if(arr[i]==element)
+			{
+				count++;
+				
+			}
+		}
+		if(count==0)
+		cout<<"element not found";
+		else
+		cout<<element<<"-"<<count;
+		
+	}
+}
